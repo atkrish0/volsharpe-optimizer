378d11efbdcde4e8ee0ff9efc147b4a394d3cea5
atkrish0 <atheesh.krishnan@outlook.com>
2023-11-11 12:04:35 -0500

    updated main with data handler functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include "MarketData.hpp"
+#include "DataHandler.h"
 #include "Statistics.h"
 
 using namespace std;
 
 int main() {
+
+    // Read stock prices from CSV
+    std::string filePath = "path/to/your/csvfile.csv"; // Update with the actual file path
+    int numberOfRows = 1000; // Update with the actual number of rows
+    int numberOfColumns = 5; // Assuming 5 stocks
+    Eigen::MatrixXd portfolio = DataHandler::readCSV(filePath, numberOfRows, numberOfColumns);
+
+    // Calculate daily returns, mean returns, etc. as before
+    Eigen::MatrixXd returns = Statistics::calculateReturns(portfolio);
+    Eigen::VectorXd meanReturns = Statistics::meanDailyReturns(returns);
+    Eigen::MatrixXd covMatrix = Statistics::covarianceMatrix(returns);
+    Eigen::VectorXd volatility = Statistics::volatility(returns);
+    Eigen::MatrixXd corrMatrix = Statistics::correlationMatrix(returns);
+
+    // ========================
     std::string filename = "market_data.csv"; // Your local CSV file with closing prices
     MarketData marketData(filename);
 
     Eigen::MatrixXd covMatrix = marketData.getCovarianceMatrix();
     std::cout << "Covariance Matrix:\n" << covMatrix << std::endl;
-    
+    // =========================
+
     // Assuming 'portfolio' is an Eigen::MatrixXd containing the stock prices
     Eigen::MatrixXd portfolio; // Populate this with your data
 

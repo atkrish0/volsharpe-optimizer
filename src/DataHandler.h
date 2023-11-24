27a937f059c7e49a7726d1489fca4bdd7ff8ca4e
atkrish0 <atheesh.krishnan@outlook.com>
2023-11-23 19:47:07 -0500

    data handler structure formatting

--- src/DataHandler.h
+++ src/DataHandler.h
@@ -3,9 +3,12 @@
 #include <vector>
 
 class DataHandler {
-public:
-    static Eigen::MatrixXd readCSV(const std::string& file, int rows, int cols);
+    public:
+        static Eigen::MatrixXd readCSV(const std::string& file, int rows, int cols);
 
-private:
-    static std::vector<std::string> split(const std::string& s, char delimiter);
+    protected:
+        
+
+    private:
+        static std::vector<std::string> split(const std::string& s, char delimiter);
 };

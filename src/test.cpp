22aacedc0140dcf67ec84d747528379b49ff8614
atkrish0 <atheesh.krishnan@outlook.com>
2023-11-23 21:49:36 -0500

    path changes to resolve eigen import errors

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x = 4, y = 6;
+
+    if (x > 5) {
+        if (y > 5)
+            cout << "x & y are > 5";
+    }
+    else
+        cout << "x <= 5";
+}

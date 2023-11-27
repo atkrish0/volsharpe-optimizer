364ccdb5d1990e5adf5cf9b5124e3645f599f8fc
atkrish0 <atheesh.krishnan@outlook.com>
2023-11-27 18:49:50 -0500

    added code for portfolio optimizer call in main

--- src/main.cpp
+++ src/main.cpp
@@ -26,5 +26,26 @@ int main() {
     std::cout << "Volatility:\n" << volatility << "\n\n";
     std::cout << "Correlation Matrix:\n" << corrMatrix << "\n\n";
 
+    // Set the number of random portfolio weights to generate
+    int numPortfolios = 50000;
+
+    // Call the PortfolioOptimizer to optimize portfolios
+    std::vector<PortfolioResult> optimizedPortfolios = PortfolioOptimizer::optimizePortfolio(
+        meanReturns, covMatrix, numPortfolios, numberOfColumns);
+
+    // Example: Print the results of the first optimized portfolio
+    if (!optimizedPortfolios.empty()) {
+        const PortfolioResult& firstPortfolio = optimizedPortfolios.front();
+        std::cout << "First Optimized Portfolio:" << std::endl;
+        std::cout << "Return: " << firstPortfolio.return_ << std::endl;
+        std::cout << "Volatility: " << firstPortfolio.volatility << std::endl;
+        std::cout << "Sharpe Ratio: " << firstPortfolio.sharpeRatio << std::endl;
+        std::cout << "Weights: ";
+        for (double weight : firstPortfolio.weights) {
+            std::cout << weight << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }

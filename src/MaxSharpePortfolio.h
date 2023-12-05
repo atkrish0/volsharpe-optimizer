31341d98bdcd700788697243963a324d010f10dd
atkrish0 <atheesh.krishnan@outlook.com>
2023-12-04 19:33:42 -0500

    added some inheritance modules

---
+++ src/MaxSharpePortfolio.h
@@ -0,0 +1,14 @@
+#ifndef MAXSHARPEPORTFOLIO_H
+#define MAXSHARPEPORTFOLIO_H
+
+#include "PortfolioStrategy.h"
+
+class MaxSharpePortfolio : public PortfolioStrategy {
+public:
+    MaxSharpePortfolio() = default;
+
+    // Implementation specific to Max Sharpe Ratio strategy
+    PortfolioResult executeStrategy(const std::vector<PortfolioResult>& portfolios) override;
+};
+
+#endif // MAXSHARPEPORTFOLIO_H
---
+++ src/PortfolioStrategy.h
@@ -0,0 +1,18 @@
+#ifndef PORTFOLIOSTRATEGY_H
+#define PORTFOLIOSTRATEGY_H
+
+#include <vector>
+#include "PortfolioOptimizer.h"
+
+class PortfolioStrategy {
+public:
+    PortfolioStrategy() = default;
+    virtual ~PortfolioStrategy() = default;
+
+    // Method to execute the strategy
+    virtual PortfolioResult executeStrategy(const std::vector<PortfolioResult>& portfolios) = 0;
+
+    // Other common methods or properties...
+};
+
+#endif // PORTFOLIOSTRATEGY_H
